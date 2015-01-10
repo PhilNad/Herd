b95db8729b086d2b6cdae0e6fedbc519a9f53d35
PhilNad <philippe.nadeau1991@gmail.com>
2015-01-10 16:58:59 -0500

    Added files for decision-making

---
+++ Herd/Decisions.cpp
@@ -0,0 +1,3 @@
+#include "Decisions.h"
+
+//Will take care of all decision-making processes most notably when we will need to move through the board.
---
+++ Herd/Decisions.h
@@ -0,0 +1,7 @@
+#include "Arduino.h"
+
+
+#ifndef DECISIONS_H
+#define DECISIONS_H
+
+#endif //DECISIONS_H
